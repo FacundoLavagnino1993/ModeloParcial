cf22a035454a63b3cf1a8fe14ec3969bc8c51494
FacundoLavagnino1993 <facu9396@gmail.com>
2016-04-20 10:29:15 -0300

    ModeloParcial

---
+++ Repaso/Cliente.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repaso
+{
+    class Cliente
+    {
+        /*creaAlias() : Crea el alias concatenando un numero random de 4 cifras con la nacionalidad y el tipo de cliente,
+         *invocarlo a este metodo en el constructor por defecto.
+         *getAlias() : Retorna el alias.
+         *retornarDatos(De instancia y privado) : Retorna un string con todos los datos
+         *retornarDatos(estatico y publico) : Retorna un string con todos los datos, utilizar el metodo de instancia.
+         */
+
+        private string _aliasParaIncognito;
+        private string _nacionalidad;
+        private string _nombre;
+        private Enum.eTipoCliente _tipoCliente;
+
+        private static Cliente()
+        { 
+        
+        }
+
+        public Cliente(Enum.eTipoCliente cliente, string nombre)
+        { 
+        
+        }
+
+        public Cliente(Enum.eTipoCliente cliente, string nombre, string nacionalidad)
+        { 
+        
+        }
+        
+        public Cliente(string nombre)
+        {
+ 
+        }
+    }
+}
---
+++ Repaso/cuentaOffShore.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repaso
+{
+    class cuentaOffShore
+    {
+        /* En esta clase el constructor por defecto debera sumar uno al atributo estatico cantidad de cuentas
+         * y el lugar por defecto sera bahamas, no repetir lineas de codigo. Constructor de clase en esta clase
+         * sera el encargado de inicializar la cantidad de cuentas a cero 0. */
+
+        private Enum.eTipoDeBeneficio beneficio;
+        public  static int CantidadDeCuentas = 0;
+        public List<Cliente> ListadoClientes;
+        private Enum.eParaisosFiscales lugar;
+
+         public cuentaOffShore()
+        {
+            CantidadDeCuentas = 1;
+            this.lugar = Enum.eParaisosFiscales.Bahamas;
+        }
+
+         private static cuentaOffShore()
+        {
+            CantidadDeCuentas = 0;   
+        }
+
+
+
+        public cuentaOffShore(Enum.eParaisosFiscales lugar)
+        {
+            this.lugar = lugar;
+        }
+
+        public cuentaOffShore(Enum.eParaisosFiscales lugar, Enum.eTipoDeBeneficio beneficio)
+        {
+            this.lugar = lugar;
+            this.beneficio = beneficio;
+        }
+
+
+    }
+}
+
+namespace Enum
+{
+    public enum eParaisosFiscales
+    {
+       Anguila, Bahamas, Belice, Delaware, Gibraltar, Hong_Kong, Mauricio, Panama, Seychelles, Singapur, Suiza
+    }
+
+    public enum eTipoProducto
+    {
+        Cuentas_Bancarias, Polizas_de_Seguros, Inversiones_Inmobiliarias, Sociedades_Extranjeras, Fondos_de_Inversion
+    }
+
+    public enum eTipoDeBeneficio
+    {
+        Secreto_Bancario, Beneficios_Fiscales, Secreto_Bancario_Y_Beneficios_Fiscales
+    }
+
+    public enum eTipoCliente
+    {
+        Politico_Corrupto, Empresario_Corrupto, JugadorDeFutbol, financista, maestroRural
+    }
+
+}
